4a4db58057fc548b1ae09158177401c1fc2ce313
pooya.nasiri75 <pooya.nasiri75@gmail.com>
2024-05-09 20:27:05 +0200

    Changed inputs from tk.entry to tk datepicker

--- life.py
+++ life.py
@@ -1,8 +1,9 @@
 import datetime
 import tkinter as tk
+from tkcalendar import Calendar
 
 
-def display_life_calendar():
+def display_life_calendar(year, month, day, lifespan):
     weeks_lived = (datetime.date.today() -
                    datetime.date(year, month, day)).days // 7
     square_size = 10
@@ -17,31 +18,8 @@ def display_life_calendar():
             x = j * square_size * space_size + 2
             y = i * square_size * space_size + 2
             canvas.create_rectangle(x, y, x + square_size, y + square_size, fill=(
-                "black"if i * 52 + j < weeks_lived else "white"), outline="black")
+                "black" if i * 52 + j < weeks_lived else "white"), outline="black")
     root.mainloop()
-
-
-def make_label(root, text, row, column):
-    label = tk.Label(root, text=text)
-    label.grid(row=row, column=column, padx=pad, pady=pad)
-    return label
-
-
-# def make_entry(root, default_text, row, column):
-#     entry = tk.Entry(root)
-#     entry.insert(0, default_text)
-#     entry.grid(row=row, column=column, padx=pad, pady=pad)
-#     return int(entry.get())
-
-def make_entry(root, default_text, row, column, max_lenght=4):
-    def validate_input(text):
-        return (text.isdigit() and len(text) <= max_lenght) or text == ""
-
-    entry = tk.Entry(root, validate="key", validatecommand=(
-        root.register(validate_input), '%P'))
-    entry.insert(0, default_text)
-    entry.grid(row=row, column=column, padx=pad, pady=pad)
-    return int(entry.get())
 
 
 def make_button(root, text, row, column, command):
@@ -50,18 +28,26 @@ def make_button(root, text, row, column, command):
     return button
 
 
-# GUI setup
+def on_date_selected():
+    selected_date = date_picker.get_date().split('/')
+    year, month, day = int(
+        selected_date[2]), int(selected_date[0]), int(selected_date[1])
+    if year > datetime.date.today().year or (year == datetime.date.today().year and month > datetime.date.today().month) or (year == datetime.date.today().year and month == datetime.date.today().month and day > datetime.date.today().day):
+        print("Please select a date in the past")
+        return
+    display_life_calendar(year, month, day, 80)
+
+
 root = tk.Tk()
-root.title("Life Calendar Setup")
-root.geometry("260x190")
+root.title("Life Calendar")
+root.geometry("265x280")
 pad = 7
-make_label(root, "Birth Year:", 0, 0)
-year = make_entry(root, "2000", 0, 1, 4)
-make_label(root, "Birth Month:", 1, 0)
-month = make_entry(root, "1", 1, 1, 2)
-make_label(root, "Birth Day:", 2, 0)
-day = make_entry(root, "1", 2, 1, 2)
-make_label(root, "Lifespan (years):", 3, 0)
-lifespan = make_entry(root, "80", 3, 1, 3)
-make_button(root, "Draw Calendar", 4, 2, display_life_calendar)
+
+label = tk.Label(root, text="Select Your Birthdate")
+label.grid(row=0, column=0, padx=pad, pady=pad)
+date_picker = Calendar(root, selectmode="day", year=1996,
+                       month=11, day=10, date_pattern="mm/dd/yyyy")
+date_picker.grid(row=1, column=0, padx=pad, pady=pad)
+draw_button = make_button(
+    root, "Show My Life Calender", 2, 2, on_date_selected)
 root.mainloop()
